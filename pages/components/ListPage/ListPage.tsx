a5f8734c77561eb4338d783188fddf8e7b22eb6b
Olga Mishareva <mishareva.olga@gmail.com>
2023-04-20 19:51:38 +0200

    feat: add link from list to form page

--- pages/components/ListPage/ListPage.tsx
+++ pages/components/ListPage/ListPage.tsx
@@ -1,4 +1,6 @@
 import { FC } from "react";
+import Link from "next/link";
+
 import { db } from "@/db";
 import styles from "./ListPage.module.css";
 import { Card } from "../../../helpers/intefaces";
@@ -6,20 +8,25 @@ import { Card } from "../../../helpers/intefaces";
 const ListPage: FC = () => {
   console.log(db);
   return (
-    <ul className={styles.cardList}>
-      {db.map((item: Card, i) => (
-        <li className={styles.card} key={i}>
-          <ol className="events-list">
-            <p>{item.date}</p>
-            {item.dayEvents.map((dayEvent: String, i) => (
-              <li className="day-event" key={i}>
-                {dayEvent}
-              </li>
-            ))}
-          </ol>
-        </li>
-      ))}
-    </ul>
+    <div className={styles.wrapper}>
+      <Link className={styles.link} href="/create">
+        I liked it
+      </Link>
+      <ul className={styles.cardList}>
+        {db.map((item: Card, i) => (
+          <li className={styles.card} key={i}>
+            <ol className={styles.eventList}>
+              <p>{item.date}</p>
+              {item.dayEvents.map((dayEvent: String, i) => (
+                <li className="day-event" key={i}>
+                  {dayEvent}
+                </li>
+              ))}
+            </ol>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 };
 
