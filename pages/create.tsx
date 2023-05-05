2c2407053ed51cbcd18e4c0c3973d53dd1182cbe
Olga Mishareva <mishareva.olga@gmail.com>
2023-05-05 11:55:50 +0200

    feat: add new paths for create

--- pages/create.tsx
+++
@@ -1,9 +0,0 @@
-import { FC } from "react";
-import type { NextPage } from "next";
-import FormPage from "../components/FormPage/FormPage";
-
-const CreatePage: NextPage = () => {
-  return <FormPage />;
-};
-
-export default CreatePage;
