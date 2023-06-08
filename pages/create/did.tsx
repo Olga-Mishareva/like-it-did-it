f171ad19ea989d7e7e675e64b85f01902a685712
Olga Mishareva <mishareva.olga@gmail.com>
2023-06-08 16:21:27 +0200

    feat: pass props to components

--- pages/create/did.tsx
+++ pages/create/did.tsx
@@ -2,11 +2,21 @@ import type { NextPage } from "next";
 import { usePathname } from "next/navigation";
 
 import FormPage from "../../components/FormPage/FormPage";
+import { ICard } from "../../helpers/intefaces";
 
-const CreatePage: NextPage = () => {
+type nextPageProp = {
+  onAddCard(card: ICard): void;
+};
+
+const CreatePage: NextPage<nextPageProp> = ({ onAddCard }) => {
   const pathname = usePathname();
 
-  return <FormPage path={pathname.endsWith("liked") ? "liked" : "did"} />;
+  return (
+    <FormPage
+      path={pathname.endsWith("liked") ? "liked" : "did"}
+      onAddCard={onAddCard}
+    />
+  );
 };
 
 export default CreatePage;
--- pages/list/did.tsx
+++ pages/list/did.tsx
@@ -2,11 +2,21 @@ import type { NextPage } from "next";
 import { usePathname } from "next/navigation";
 
 import ListPage from "../../components/ListPage/ListPage";
+import { ICard } from "../../helpers/intefaces";
 
-const HomePage: NextPage = () => {
+type nextPageProp = {
+  cards: ICard[];
+};
+
+const HomePage: NextPage<nextPageProp> = ({ cards }) => {
   const pathname = usePathname();
 
-  return <ListPage path={pathname.endsWith("liked") ? "liked" : "did"} />;
+  return (
+    <ListPage
+      path={pathname.endsWith("liked") ? "liked" : "did"}
+      cards={cards}
+    />
+  );
 };
 
 export default HomePage;
