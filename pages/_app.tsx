535cacc1c2e65b4669fbe0076e6e9f4cfdddde07
Olga Mishareva <mishareva.olga@gmail.com>
2023-06-08 16:18:43 +0200

    feat: add card to array & save cards-array to local storage

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,31 +1,31 @@
-import { FC, ReactNode } from "react";
+import { FC } from "react";
 import "@/styles/globals.css";
 import Head from "next/head";
-import type { NextPage } from "next";
 import type { AppProps } from "next/app";
 import { Inter } from "next/font/google";
+import useLocalStorageState from "use-local-storage-state";
 
 import Layout from "../components/Layout/Layout";
+import { ICard } from "../helpers/intefaces";
 
 const inter = Inter({ subsets: ["latin"], variable: "--inter-font" });
 
-// export type PageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
-//   page: ReactNode;
-// };
-
-// export type AppPropsWithLayout = AppProps & {
-//   page: React.ReactNode;
-//   // pageProps: AppProps;
-// };
-
 const App: FC<AppProps> = ({ Component, pageProps }) => {
+  const [cards, setCards] = useLocalStorageState<ICard[]>("cards", {
+    defaultValue: [],
+  });
+
+  function handleCards(card: ICard) {
+    setCards([card, ...cards]);
+  }
+
   return (
     <div className={inter.className}>
       <Head>
         <title>Liked it - Did it</title>
       </Head>
       <Layout>
-        <Component {...pageProps} />
+        <Component {...pageProps} cards={cards} onAddCard={handleCards} />
       </Layout>
     </div>
   );
