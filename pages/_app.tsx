efb8932fb8df688d8a6c6ce3afa6b892bcbcd3ec
Olga Mishareva <mishareva.olga@gmail.com>
2023-06-15 15:05:02 +0200

    feat: add edit-current-day & add-new-day functions

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,10 @@
-import { FC } from "react";
+import { FC, useState, useEffect } from "react";
 import "@/styles/globals.css";
 import Head from "next/head";
 import type { AppProps } from "next/app";
 import { Inter } from "next/font/google";
 import useLocalStorageState from "use-local-storage-state";
+import { usePathname } from "next/navigation";
 
 import Layout from "../components/Layout/Layout";
 import { ICard } from "../helpers/intefaces";
@@ -11,21 +12,39 @@ import { ICard } from "../helpers/intefaces";
 const inter = Inter({ subsets: ["latin"], variable: "--inter-font" });
 
 const App: FC<AppProps> = ({ Component, pageProps }) => {
+  const pathname = usePathname();
+
   const [cards, setCards] = useLocalStorageState<ICard[]>("cards", {
     defaultValue: [],
   });
 
   function handleCards(newCard: ICard) {
-    if (cards[0]) {
-      if (cards.filter((item) => item.date === newCard.date)) {
-        const refreshedCards = cards.map((item) => {
-          return item.date === newCard.date
-            ? { ...item, dayEvents: item.dayEvents.concat(newCard.dayEvents) }
-            : item;
-        });
-        setCards(refreshedCards);
-      } else setCards([newCard, ...cards]);
-    } else setCards([newCard]);
+    const path = pathname.endsWith("liked") ? "liked" : "done";
+    const currentDay = cards.some(
+      (card) => card.date === newCard.date && card.status === path
+    );
+
+    if (currentDay) {
+      editCurrentDayCard(newCard);
+    } else {
+      addNewDayCard(newCard);
+    }
+  }
+
+  function editCurrentDayCard(newCard: ICard) {
+    const refreshedCards = cards.map((card) => {
+      if (card.date === newCard.date && card.status === newCard.status) {
+        return {
+          ...card,
+          dayEvents: [...card.dayEvents, ...newCard.dayEvents],
+        };
+      } else return card;
+    });
+    setCards(refreshedCards);
+  }
+
+  function addNewDayCard(newCard: ICard) {
+    setCards([newCard, ...cards]);
   }
 
   return (
@@ -34,7 +53,12 @@ const App: FC<AppProps> = ({ Component, pageProps }) => {
         <title>Liked it - Done it</title>
       </Head>
       <Layout>
-        <Component {...pageProps} cards={cards} onAddCard={handleCards} />
+        <Component
+          {...pageProps}
+          path={pathname.endsWith("liked") ? "liked" : "done"}
+          cards={cards}
+          onAddCard={handleCards}
+        />
       </Layout>
     </div>
   );
